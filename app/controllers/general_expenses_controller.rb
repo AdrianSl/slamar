868f03a51187357b3e3f2d8a4a43501690e8d1ee
AdrianSl <slaby.adrian@gmail.com>
2015-05-27 12:17:08 +0200

    Add GeneralExpenses controller

---
+++ app/controllers/general_expenses_controller.rb
@@ -0,0 +1,4 @@
+class GeneralExpensesController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'general_expenses/new'
+
   devise_for :users
   root 'dashboard#index'
 end
