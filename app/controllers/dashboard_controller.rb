8aaf0467a04c7f66779de290b988ef51d381f05c
AdrianSl <slaby.adrian@gmail.com>
2015-06-18 17:25:47 +0200

    Add General Expenses to Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
   before_action :logged_in_user
 
   def index
+    @general_expenses = GeneralExpense.select("name, value").where(date: DateTime.now).order(name: :asc)
   end
 end
