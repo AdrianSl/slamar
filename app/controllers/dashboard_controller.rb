593be8782615220007b711e73f90b8f20c49538d
AdrianSl <slaby.adrian@gmail.com>
2015-06-20 16:41:01 +0200

    Add last days selection in dashbord

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,37 @@ class DashboardController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @general_expenses = GeneralExpense.today.decorate
-    @incomes_truck_routes = Income.truck_routes.today.decorate
-    @incomes_brand_shops = Income.brand_shops.today.decorate
-    @incomes_external_shops = Income.external_shops.today.decorate
+    @chosen_date = give_date(params[:date])
+    @query_dates = Income.select(:date).distinct.limit(6).order('date DESC').reverse
+    @dates = Array.new()
+    @query_dates.each do |item|
+      @dates << { active: is_chosen_date?(item.date, @chosen_date), 
+                  date_name: name_of_day(item.date),
+                  date_value: item.date.to_s(:number) 
+                }
+    end
+    @general_expenses = GeneralExpense.where(date: @chosen_date).decorate
+    @incomes_truck_routes = Income.truck_routes.where(date: @chosen_date).decorate
+    @incomes_brand_shops = Income.brand_shops.where(date: @chosen_date).decorate
+    @incomes_external_shops = Income.external_shops.where(date: @chosen_date).decorate
+    @chosen_date = @chosen_date.strftime("%Y-%m-%d, %A") 
   end
+
+  private
+
+    def give_date(param)
+      param.nil? ? DateTime.now : param.to_date 
+    end
+    def is_chosen_date?(any_date, chosen_date)
+      any_date == chosen_date.to_date ? true : false
+    end
+    def name_of_day(any_date)
+      if any_date == DateTime.now.to_date
+       any_date.strftime("%a") + ', Today'
+      elsif any_date  == (DateTime.now - 1.day).to_date
+        any_date.strftime("%a") + ', Yesterday'
+      else
+        any_date.strftime("%a, %m-%d")
+      end
+    end
 end
