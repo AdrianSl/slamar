1037832054d94bfa121a1bda6a6811a8e9760f65
AdrianSl <slaby.adrian@gmail.com>
2015-06-21 17:45:17 +0200

    Add validation to income model

--- app/models/income.rb
+++ app/models/income.rb
@@ -3,6 +3,7 @@ class Income < ActiveRecord::Base
   belongs_to :external_shop
   belongs_to :truck_route
   belongs_to :driver
+  validates :value, :date,  presence: true
 
   scope :today, -> {where(date: DateTime.now)}
   scope :truck_routes, -> {joins(:truck_route).where.not('truck_route_id' => nil).order('truck_routes.name')}
