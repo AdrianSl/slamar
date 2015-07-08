2e9cddf29207746907df6bc6f99f7739af168ec3
AdrianSl <slaby.adrian@gmail.com>
2015-07-08 16:27:03 +0200

    Finish refactor of income model assocciation

--- app/assets/javascripts/incomes.js
+++ app/assets/javascripts/incomes.js
@@ -1,22 +1,11 @@
+$(function () {
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $("select#income_income_source_id").val("");
+  });      
+});
 function edit(id) {
   $('#' + id + '_driver_disabled').prop('disabled', false);
   $('#' + id + '_value_disabled').prop('disabled', false);
   $('#' + id + '_midday_value_disabled').prop('disabled', false);
   $('#' + id + '_link_disabled').hide();
 }
-
-$(':input[id=income_income_source_type_truckroute]').change(function() {
-   $("#brand_shops_option").removeClass("none");
-   $("#external_shops_option").removeClass("none");
-   $("#truck_routes_option").addClass("none");
-});
-$(':input[id=income_income_source_type_brandshop]').change(function() {
-   $("#brand_shops_option").addClass("none");
-   $("#external_shops_option").removeClass("none");
-   $("#truck_routes_option").removeClass("none");
-});
-$(':input[id=income_income_source_type_externalshop]').change(function() {
-   $("#brand_shops_option").removeClass("none");
-   $("#external_shops_option").addClass("none");
-   $("#truck_routes_option").removeClass("none");
-});
--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -3,21 +3,36 @@ class IncomesController < ApplicationController
 
   def new
     @income = Income.new
+    @income_types = get_income_types
     @drivers = Driver.all.map{|c| [ "#{c.name} #{c.surname}", c.id ]}
     @truck_routes = TruckRoute.all.map{|c| [ c.name, c.id ]}
     @brand_shops = BrandShop.all.map{|c| [ c.name, c.id ]}
     @external_shops = ExternalShop.all.map{|c| [ c.name, c.id ]}
   end
   def create
-    @income = Income.new(income_params)
+    unless params[:income][:income_source_id].empty?
+      if params[:income][:income_source_type] == "TruckRoute"
+        income_source = TruckRoute.find(params[:income][:income_source_id])
+      elsif params[:income][:income_source_type] == "BrandShop"
+        income_source = BrandShop.find(params[:income][:income_source_id])
+      elsif params[:income][:income_source_type] == "ExternalShop"
+        income_source = ExternalShop.find(params[:income][:income_source_id])
+      end
+    end
+    if income_source.nil?
+      @income = Income.new(income_params)
+    else
+      @income = income_source.incomes.build(income_params)
+    end
     if @income.save
       flash[:success] = "Income added successfully."
       redirect_to dashboard_path(date_to_url(income_params[:date]))
     else
-    @drivers = Driver.all.map{|c| [ "#{c.name} #{c.surname}", c.id ]}
-    @truck_routes = TruckRoute.all.map{|c| [ c.name, c.id ]}
-    @brand_shops = BrandShop.all.map{|c| [ c.name, c.id ]}
-    @external_shops = ExternalShop.all.map{|c| [ c.name, c.id ]}
+      @income_types = get_income_types
+      @drivers = Driver.all.map{|c| [ "#{c.name} #{c.surname}", c.id ]}
+      @truck_routes = TruckRoute.all.map{|c| [ c.name, c.id ]}
+      @brand_shops = BrandShop.all.map{|c| [ c.name, c.id ]}
+      @external_shops = ExternalShop.all.map{|c| [ c.name, c.id ]}
       render 'new'
     end
   end
@@ -33,7 +48,19 @@ class IncomesController < ApplicationController
 
   private
 
-  def income_params
-    params.require(:income).permit(:value, :midday_value, :date, :income_source_id, :income_source_type, :driver_id)
-  end
+    def is_chosen_type?(type)
+      if params[:income].nil? 
+        type == "TruckRoute" ? true : false
+      else
+        type == params[:income][:income_source_type] ? true : false
+      end
+    end
+    def get_income_types
+      [{ name: "Truck Route", active: is_chosen_type?("TruckRoute"), pane: "truck_route"},
+       { name: "Brand Shop", active: is_chosen_type?("BrandShop"), pane: "brand_shop"},
+       { name: "External Shop", active: is_chosen_type?("ExternalShop"), pane: "external_shop"}] 
+    end
+    def income_params
+      params.require(:income).permit(:value, :midday_value, :date, :driver_id)
+    end
 end
