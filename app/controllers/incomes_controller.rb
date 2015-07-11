fa1d0cf29cab2bb54ff3fe09ec183764ea78cdff
AdrianSl <slaby.adrian@gmail.com>
2015-07-11 14:39:42 +0200

    Add index, edit, update actions to incomes

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,6 +1,18 @@
 class IncomesController < ApplicationController
   before_action :logged_in_user
-
+  def index
+    if !params[:from].blank?
+      @from = params[:from]
+    else 
+      @from = (Time.now - 1.month).strftime("%Y-%m-%d")
+    end
+    if !params[:to].blank?
+      @to = params[:to]
+    else 
+      @to = Time.now.strftime("%Y-%m-%d")
+    end
+    @incomes = Income.all_types.where(date: @from..@to).page(params[:page])
+  end
   def new
     @income = Income.new
     @income_types = get_income_types
@@ -36,6 +48,29 @@ class IncomesController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @income = Income.find(params[:id])
+    @income_types = get_income_types
+    @drivers = Driver.all.map{|c| [ "#{c.name} #{c.surname}", c.id ]}
+    @truck_routes = TruckRoute.all.map{|c| [ c.name, c.id ]}
+    @brand_shops = BrandShop.all.map{|c| [ c.name, c.id ]}
+    @external_shops = ExternalShop.all.map{|c| [ c.name, c.id ]}
+  end
+  def update
+    @income = Income.find(params[:id])
+    if @income.update_attributes(income_params)
+      flash[:success] = "Income updated successfully."
+      redirect_to dashboard_path(date_to_url(income_params[:date]))
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    income = Income.find(params[:id])
+    income.destroy
+    flash[:success] = "Income removed successfully."
+    redirect_to incomes_path
+  end
   def update_multiple
     if params[:incomes].nil?
       flash[:alert] = "There is no data to save"
@@ -50,7 +85,11 @@ class IncomesController < ApplicationController
 
     def is_chosen_type?(type)
       if params[:income].nil? 
-        type == "TruckRoute" ? true : false
+        if @income.income_source_type.blank?
+          type == "TruckRoute" ? true : false
+        else
+          type == @income.income_source_type ? true : false
+        end  
       else
         type == params[:income][:income_source_type] ? true : false
       end
@@ -61,6 +100,6 @@ class IncomesController < ApplicationController
        { name: "External Shop", active: is_chosen_type?("ExternalShop"), pane: "external_shop"}] 
     end
     def income_params
-      params.require(:income).permit(:value, :midday_value, :date, :driver_id)
+      params.require(:income).permit(:value, :midday_value, :date, :driver_id, :income_source_id)
     end
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -2,7 +2,9 @@ class Income < ActiveRecord::Base
   belongs_to :income_source, polymorphic: true
   belongs_to :driver
   validates :date, :income_source_id,  presence: true
+  paginates_per 50
 
+  scope :all_types, -> {joins("LEFT JOIN external_shops ON incomes.income_source_id=external_shops.id AND incomes.income_source_type='ExternalShop'").joins("LEFT JOIN external_shops ON incomes.income_source_id=external_shops.id AND incomes.income_source_type='ExternalShop'").joins("LEFT JOIN brand_shops ON incomes.income_source_id=brand_shops.id AND incomes.income_source_type='BrandShop'").where.not('value' => nil).order(date: :desc, income_source_type: :asc)}
   scope :external_shops, -> {joins("LEFT JOIN external_shops ON incomes.income_source_id=external_shops.id AND incomes.income_source_type='ExternalShop'").where('income_source_type' => 'ExternalShop').order('external_shops.name')}
   scope :truck_routes, -> {joins("LEFT JOIN truck_routes ON incomes.income_source_id=truck_routes.id AND incomes.income_source_type='TruckRoute'").where('income_source_type' => 'TruckRoute').order('truck_routes.name')}
   scope :brand_shops, -> {joins("LEFT JOIN brand_shops ON incomes.income_source_id=brand_shops.id AND incomes.income_source_type='BrandShop'").where('income_source_type' => 'BrandShop').order('brand_shops.name')}
