3a6a9dd2ecea8d601bbaa920bd6062e3baede038
AdrianSl <slaby.adrian@gmail.com>
2015-07-14 18:17:45 +0200

    Fix bug with Brand shops edit in dashboard

--- app/decorators/income_decorator.rb
+++ app/decorators/income_decorator.rb
@@ -7,6 +7,6 @@ class IncomeDecorator < Draper::Decorator
     number_with_precision object.value, precision: 2
   end
   def midday_value
-    number_with_precision object.value, precision: 2
+    number_with_precision object.midday_value, precision: 2
   end
 end
